67efcf5bb039ad4416ba5a9de1b11c244fdfc4c9
andy741217 <76022969+andy741217@users.noreply.github.com>
2021-03-29 08:24:17 +0800

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -46,11 +46,11 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.deadzoneBP = [0., .5]
     ret.longitudinalTuning.deadzoneV = [0.00, 0.00]
     ret.gasMaxBP = [0., 1., 1.1, 15., 40.]
-    ret.gasMaxV = [2., 2., 2., 1.68, 1.3]
+    ret.gasMaxV = [1.5, 1.5, 1.,5 1.28, 1.1]
     ret.brakeMaxBP = [0., 5., 5.1]
-    ret.brakeMaxV = [3.5, 3.5, 3.5]
+    ret.brakeMaxV = [2.0, 2.0, 2.0]
     
-    ret.maxSteeringAngleDeg = 210.
+    ret.maxSteeringAngleDeg = 1000.
     
     eps_modified = False
     for fw in car_fw:
