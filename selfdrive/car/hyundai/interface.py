296148656d31e8e257af1d01d96e7f9e2d0b6746
andy741217 <76022969+andy741217@users.noreply.github.com>
2021-04-02 11:00:27 +0800

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -40,15 +40,15 @@ class CarInterface(CarInterfaceBase):
 
     #Long tuning Params -  make individual params for cars, baseline Hyundai genesis
     ret.longitudinalTuning.kpBP = [0., .3, 10., 35.]
-    ret.longitudinalTuning.kpV = [1.62, .72, .27, .27]
+    ret.longitudinalTuning.kpV = [1.8, .8, .3, .3]
     ret.longitudinalTuning.kiBP = [0., .3, 15., 35.]
-    ret.longitudinalTuning.kiV = [0.66, .25, .22, 1.98]
+    ret.longitudinalTuning.kiV = [0.15, .055, .05, .045]
     ret.longitudinalTuning.deadzoneBP = [0., .5]
     ret.longitudinalTuning.deadzoneV = [0.00, 0.00]
-    ret.gasMaxBP = [0.]
-    ret.gasMaxV = [0.5]
-    ret.brakeMaxBP = [0.]
-    ret.brakeMaxV = [3.0]
+    ret.gasMaxBP = [0., 1., 1.1, 15., 40.]
+    ret.gasMaxV = [2., 2., 2., 1.68, 1.3]
+    ret.brakeMaxBP = [0., 5., 5.1]
+    ret.brakeMaxV = [3.5, 3.5, 3.5]
     
     ret.maxSteeringAngleDeg = 1000.
     
